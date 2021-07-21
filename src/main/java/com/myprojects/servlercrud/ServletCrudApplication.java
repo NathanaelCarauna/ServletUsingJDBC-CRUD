638c7d93a7216da6a8778897a9b58b245261977c
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-07-21 14:20:42 -0300

    Project created

---
+++ src/main/java/com/myprojects/servlercrud/ServletCrudApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.servlercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/servlercrud/ServletCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.servlercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
